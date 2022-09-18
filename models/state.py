8bca767b2a2da011498a40ba996bd652005461b7
Abdoulkarim <amahamn@gmail.com>
2022-09-18 02:39:46 +0200

    final

--- models/state.py
+++ models/state.py
@@ -17,12 +17,9 @@ class State(BaseModel, Base):
     '''
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-    if environ.get("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="delete")
-    else:
-        name = ""
-
+    cities = relationship("City", backref="state", cascade="delete")
+    
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             city_dict = models.storage.all(models.classes["City"])
