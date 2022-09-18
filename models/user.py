49a0e26e490d4fd778e1a86e581a5f98843da060
Abdoulkarim <amahamn@gmail.com>
2022-09-18 03:09:39 +0200

    final

--- models/user.py
+++ models/user.py
@@ -15,6 +15,7 @@ class User(BaseModel, Base):
 
     if environ.get("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'users'
+        
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
