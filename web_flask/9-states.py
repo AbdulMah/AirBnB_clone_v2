e3ae97d3d08445dd713b4033d0412bc31b966b71
Abdoulkarim <amahamn@gmail.com>
2022-10-17 00:53:31 +0200

    final

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask("__name__")
+
+
+@app.teardown_appcontext
+def refresh(exception):
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def route_states():
+    data = storage.all("State")
+    states = data.values()
+    return render_template('7-states_list.html', states_list=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def route_city():
+    data = storage.all("State")
+    states = data.values()
+    return render_template('8-cities_by_states.html', states_list=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
